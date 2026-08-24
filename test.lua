cf9a2dc5a2b5235deaa1ec3a6b2529a4fc5c30f4
Orvion <44488691+KnullXDgt@users.noreply.github.com>
2026-08-24 09:38:59 +0800

    feat: add WelcomeCard to Info tab in test.lua

--- test.lua
+++ test.lua
@@ -1236,6 +1236,7 @@ local Window = Orvion:CreateWindow({
 -- ====== FISHING TAB ======
 -- ====== INFO TAB ======
 local InfoTab = Window:CreateTab("Info", "rbxassetid://94529541997278")
+Window:AddWelcomeCard(InfoTab)
 local InfoSection = Window:AddCollapsible(InfoTab, "Information", true)
 Window:AddParagraph(InfoSection, "What is Orvion Hub?", "Orvion Hub is a reflection of my coding journey  built through trial, error, and a lot of iteration. It shows how much I have grown as a developer, and how much I still have left to learn.\nLowkey started this just for myself, no cap. Somewhere along the way it turned into something worth sharing.")
 
